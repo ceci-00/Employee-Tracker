77173c13248fcc61e496c22269f3369e90149135
ceci-00 <ceci.ga003@gmail.com>
2024-04-26 21:28:44 -0500

    created database with department, role, and employee tables

---
+++ assets/db/schema.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS departments_db;
+CREATE DATABASE departments_db;
+
+USE departments_db;
+
+CREATE TABLE department (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30)
+);
+
+CREATE TABLE role (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL(10, 2),
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT NULL,
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
+
+SELECT * FROM department;
+SELECT * FROM role;
+SELECT * FROM employee;
